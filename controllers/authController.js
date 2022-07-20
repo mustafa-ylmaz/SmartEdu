9299bacb11d427585b434f65005bc02018bcbbd0
Mustafaylmaz <mustafaa.ylmaz@gmail.com>
2022-07-20 21:24:52 +0300

    role middleware added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,58 +1,54 @@
-const User = require('../models/User');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 exports.createUser = async (req, res) => {
-    console.log(req.body);
-    const user = await User.create(req.body);
+  console.log(req.body);
 
-    try {
-        res.status(201).json({
-            user,
-            status: 'success'
-        })
-    } catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err
-        });
-    }
+  try {
+    const user = await User.create(req.body);
+    res.status(201).redirect('/');
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-exports.loginUser =  (req, res) => {
-    try {
-      const { email, password } = req.body;
-  
-       User.findOne({ email }, (err, user) => {
-        if (user) {
-          bcrypt.compare(password, user.password, (err, same) => {
-            if (same) {
-              // USER SESSION
+exports.loginUser = (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-              req.session.userID = user._id
-              res.status(200).redirect('/users/dashboard');
-            }
-          });
-        }
-      });
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        error,
-      });
-    }
-  };
+    User.findOne({ email }, (err, user) => {
+      if (user) {
+        bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
 
-
-  exports.logoutUser = (req, res) => {
-    req.session.destroy(() => {
-      res.redirect('/');
+            req.session.userID = user._id;
+            res.status(200).redirect("/users/dashboard");
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
     });
   }
+};
 
-  exports.getDashboardPage = async(req, res) => {
-    const user = await User.findById(req.session.userID);
-    res.status(200).render('dashboard', {
-      page_name: 'dashboard',
-      user
-    });
-  }
+exports.logoutUser = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findById(req.session.userID);
+  res.status(200).render("dashboard", {
+    page_name: "dashboard",
+    user,
+  });
+};
--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const courseController = require('../controllers/courseController');
+const roleMiddleware = require('../middlewares/roleMiddleware');
 const mongoose = require('mongoose');
 
-router.route('/').post(courseController.createCourse);
+
+router.route('/').post(roleMiddleware(["teacher","admin"]),courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
 router.route('/:slug').get(courseController.getCourse);
 module.exports = router
